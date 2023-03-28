4ca46dca0b06ba7629d5db9955244bb89e27b5b3
Manh <nguyenducmanhh201@gmail.com>
2023-03-28 22:44:04 +0700

    add mail template

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from './modules/user/user.module';
 import { RedisModuleCache } from './modules/redis/redis.module';
 import { RoomModule } from './modules/room/room.module';
 import { AmenitiesModule } from './modules/amenities/amenities.module';
-import { ImageKitModule } from './imageKit/imagekit.module';
+import { ImageKitModule } from './modules/imageKit/imagekit.module';
 import { BookingModule } from './modules/booking/booking.module';
 @Module({
   imports: [AuthModule, ConfigModule, DatabaseModule, RoomModule, AmenitiesModule, ImageKitModule, BookingModule],
