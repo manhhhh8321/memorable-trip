cebd45884a37de09c1f5f0f82ad72ddaeca360ac
Manh Nguyen <nguyenducmanhh201@gmail.com>
2023-04-26 10:12:31 +0700

    unit test

--- src/modules/discount/discount.controller.spec.ts
+++ src/modules/discount/discount.controller.spec.ts
@@ -1,17 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DiscountController } from './discount.controller';
 import { DiscountService } from './discount.service';
+import { RoomService } from '../room/room.service';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Amenities, Discount, Room, RoomDiscount } from 'src/entities';
+import { RoomModule } from '../room/room.module';
+import { UserModule } from '../user/user.module';
+import { City } from 'src/entities/city.entity';
 
 describe('DiscountController', () => {
   let controller: DiscountController;
+  let discountService: DiscountService;
+  let roomService: RoomService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [RoomModule, UserModule, TypeOrmModule.forFeature([Discount, RoomDiscount, Room, City])],
       controllers: [DiscountController],
-      providers: [DiscountService],
-    }).compile();
+      providers: [
+        DiscountService,
+        {
+          provide: getRepositoryToken(Discount),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(RoomDiscount),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Room),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(City),
+          useValue: {},
+        },
+      ],
+    })
+      .overrideProvider(RoomService)
+      .useValue({})
+      .compile();
 
     controller = module.get<DiscountController>(DiscountController);
+    discountService = module.get<DiscountService>(DiscountService);
+    roomService = module.get<RoomService>(RoomService);
   });
 
   it('should be defined', () => {
--- src/modules/payment/payment.controller.spec.ts
+++ src/modules/payment/payment.controller.spec.ts
@@ -1,14 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { PaymentsRepository } from './payment.repository';
+import { RedisModuleCache } from '../redis/redis.module';
 
 describe('PaymentController', () => {
   let controller: PaymentController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [RedisModuleCache],
       controllers: [PaymentController],
-      providers: [PaymentService],
+      providers: [
+        PaymentService,
+        {
+          provide: PaymentsRepository,
+          useValue: {}, // Replace with the actual instance of the repository
+        },
+      ],
     }).compile();
 
     controller = module.get<PaymentController>(PaymentController);
